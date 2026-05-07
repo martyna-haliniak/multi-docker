83ae4b19757744943ea36429112ab69033bba279
martyna <martynahaliniak@gmail.com>
2026-05-07 14:34:15 +0100

    update app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
-
+ 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -23,15 +22,13 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Fib />} />
-            <Route path="/otherpage" element={<OtherPage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Fib />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
       </div>
     </Router>
   );
 }
-
+ 
 export default App;
